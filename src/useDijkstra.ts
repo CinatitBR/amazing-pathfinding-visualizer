a32dfe5a85974b845367d3d470c0981611f5b064
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-01-06 00:31:32 -0300

    Delete unused lines

--- src/useDijkstra.ts
+++ src/useDijkstra.ts
@@ -1,9 +1,5 @@
-import { useState, useEffect } from 'react';
 import { RowListType, CellType } from './components/Board/Board';
 import { TPosition } from './App'
-
-// startPos: 0-0
-// Neighbors: 0-1 (row:col+1), 1-0 (row-1:col), (row+1:col), (row:col-1)
 
 const isNeighborValid = (neighborPos: TPosition, rowList: RowListType) => {
 
@@ -65,78 +61,6 @@ const getLowestWeightNode = (nodes: CellType[]) => {
 
   return { lowestWeightNode, lowestWeightNodeIndex};
 }
-
-class Dijkstra {
-  // The initial nodes to start the algo. They are the start node neighbors.
-  private initialNodes: CellType[];
-  private rowList: RowListType;
-  private onRowListUpdate: Function;
-
-  constructor(startPos: TPosition, rowList: RowListType, onRowListUpdate: Function) {
-    this.rowList = rowList;
-    this.onRowListUpdate = onRowListUpdate;
-
-    // Get initial nodes (start node neighbors)
-    this.initialNodes = getNeighbors(startPos, rowList);
-  }
-
-  // init() {
-  //   this.findPath(this.initialNodes);
-  // }
-
-  // findPath(nodes: CellType[]): Function | CellType | null {
-  //   console.log(nodes);
-
-  //   // Get node with lowest total weight
-  //   const { lowestWeightNode, lowestWeightNodeIndex } = getLowestWeightNode(nodes);
-  
-  //   // If it is null, all nodes were processed, there's no other node to process.
-  //   if (lowestWeightNode === null || lowestWeightNodeIndex === null)
-  //     return null;
-
-  //   // Check if lowestWeightNode is the finish node
-  //   if (lowestWeightNode.state === 'target') 
-  //   {
-  //     console.log({lowestWeightNode})
-  //     return lowestWeightNode;
-  //   }
-
-  //   // Get lowestWeightNode neighbors 
-  //   const neighbors = getNeighbors(lowestWeightNode, this.rowList);
-
-  //   // Calculate new total weight for neighbors
-  //   for (let neighbor of neighbors) {
-  //     const newTotalWeight = lowestWeightNode.totalWeight + neighbor.initialWeight;
-
-  //     // Check if new totalWeight is lower
-  //     if (newTotalWeight < neighbor.totalWeight) {
-  //       // Update neighbor totalWeight
-  //       neighbor.totalWeight = newTotalWeight;
-  //       // Update neighbor parent
-  //       neighbor.parentPos = { row: lowestWeightNode.row, col: lowestWeightNode.col } 
-  //     }
-  //   }
-
-  //   // Mark node as processed
-  //   lowestWeightNode.processed = true;
-
-  //   // Update node state
-  //   lowestWeightNode.state = 'touched'
-
-  //   // Update rowList state
-  //   this.onRowListUpdate(lowestWeightNode);
-
-  //   // Remove lowestWeightNode from nodes
-  //   nodes.splice(lowestWeightNodeIndex, 1);
-
-  //   // Set next nodes. The neighbors of the current node will
-  //   // be part of the next nodes.
-  //   const nextNodes = nodes.concat(neighbors);
-
-  //   return this.findPath(nextNodes);
-  // }
-}
-
 
 type Params = {
   startPos: TPosition,
