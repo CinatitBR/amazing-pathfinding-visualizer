ced1f1adb589013961895796d266a1af9e035f1b
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-01-07 12:57:01 -0300

    Create BoardNode component

--- src/components/BoardRow/BoardRow.tsx
+++ src/components/BoardRow/BoardRow.tsx
@@ -1,5 +1,4 @@
-import { Console } from 'console';
-import React from 'react';
+import { useMemo } from 'react';
 import { RowType, CellType } from '../Board/Board';
 import { Container } from './BoardRow.style';
 
@@ -7,35 +6,48 @@ export type Props = {
   row: RowType,
   cellSize?: number,
   mouseDownButton: string | null,
-  onCellClick: Function
+  onCellClick: Function,
 }
+
+const BoardNode = ({ cell, mouseDownButton, onCellClick }: 
+  { 
+    cell: CellType, 
+    mouseDownButton: string | null, 
+    onCellClick: Function 
+  }) => (
+  <td 
+    id={cell.id} 
+    className={cell.state}
+    onClick={e => onCellClick(e, cell)}
+    onContextMenu={e => {
+      onCellClick(e, cell)
+    }}
+    onMouseEnter={e => {
+      if (mouseDownButton) {
+        onCellClick(e, cell, mouseDownButton)
+      }
+    }}
+    onMouseLeave={e => {
+      if (mouseDownButton === 'start' || mouseDownButton === 'target') {
+        onCellClick(e, cell, mouseDownButton);
+      }
+    }}>
+  </td>
+);
 
 const BoardRow = ({ row, cellSize = 25, mouseDownButton, onCellClick }: Props) => {
   return (
     <Container cellSize={cellSize} mouseDownButton={mouseDownButton}>
       {row.map(cell => (
-        <td 
-          key={cell.id} 
-          id={cell.id} 
-          className={cell.state}
-          onClick={e => onCellClick(e, cell)}
-          onContextMenu={e => {
-            onCellClick(e, cell)
-          }}
-          onMouseEnter={e => {
-            if (mouseDownButton) {
-              onCellClick(e, cell, mouseDownButton)
-            }
-          }}
-          onMouseLeave={e => {
-            if (mouseDownButton === 'start' || mouseDownButton === 'target') {
-              onCellClick(e, cell, mouseDownButton);
-            }
-          }}>
-        </td>
+        <BoardNode
+          key={cell.id}
+          cell={cell} 
+          mouseDownButton={mouseDownButton} 
+          onCellClick={onCellClick} 
+        />
       ))}
     </Container>
-  )
+  );
 }
 
 export default BoardRow
